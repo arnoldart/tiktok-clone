7959292f315e26091c6243511df870bc27215820
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2025-12-02 06:52:38 +0000

    feat: create menu item

---
+++ app/components/MenuItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="w-full flex items-center hover:bg-gray-100 p-2.5 rounded-md">
+        <div class="flex items-center lg:mx-0 mx-auto">
+            <Icon :name="icon" :color="colorString" :size="sizeString" />
+            <span
+                :class="`text-[${colorString}]`"
+                class="lg:block hidden pl-[9px] mt-05 font-semibold text-[17px]"
+            >
+                {{ iconString }}
+            </span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps([
+    'iconString',
+    'colorString',
+    'sizeString'
+]);
+
+const { iconString, colorString, sizeString } = toRefs(props);
+
+let icon = '';
+
+if(iconString.value === 'For You') {
+    icon = 'mdi:home';
+} else if (iconString.value === 'Following') {
+    icon = 'ci:group';
+} else if (iconString.value === 'LIVE') {
+    icon = 'ri:live-line';
+}
+
+</script>
--- app/components/SideNavMain.vue
+++ app/components/SideNavMain.vue
@@ -1,8 +1,33 @@
 <template>
-    <div>
+    <div 
+        id="SideNavMain"
+        :class="route.fullPath === '/' ? 'lg:w-[310px]' : 'lg-w-[220px]'"
+        class="fixed z-20 bg-white pt-[70px] h-full lg:border-r-0 border-r border-gray-300 w-[75px] overflow-auto"
+    >
+        <div class="lg:w-full w-[7t5px] mx-auto">
+            <NuxtLink to="/">
+                <MenuItem iconString="For You" colorString="#F02C56" sizeString="30" />
+            </NuxtLink>
+            <MenuItem iconString="Following" colorString="#000000" sizeString="27" />
+            <MenuItem iconString="LIVE" colorString="#000000" sizeString="27" />
+        </div>
 
+        <div class="border-gray-300 border-b lg:ml-2 mt-2"></div>
+
+        <div class="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb-2 px-2">
+            Suggested Account
+        </div>
+
+        <div class="lg:hidden block pt-3"></div>
+
+        <div class="cursor-pointer">
+            <MenuItemFollow />
+        </div>
     </div>
 </template>
 
 <script setup>
+
+const route = useRoute();
+
 </script>
