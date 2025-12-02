8d8d5e11b9d561c4971316d5550252f5bcb330ee
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2025-12-02 04:48:52 +0000

    feat: create top nav

--- app/layouts/MainLayout.vue
+++ app/layouts/MainLayout.vue
@@ -1,10 +1,15 @@
 <template>
-    <TopNav/>
-    <div>
-        
+    <TopNav />
+    <div :class="route.fullPath === '/' ? 'max-w-[1140px]' : ''"
+        class="flex justify-between mx-auto w-full lg:px-2.5 px-0">
+        <div>
+            <SideNavMain />
+        </div>
     </div>
 </template>
 
 <script setup>
+import SideNavMain from '~/components/SideNavMain.vue';
 
+const route = useRoute();
 </script>
